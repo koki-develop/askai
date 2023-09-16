46aceed77e17f292798ddbfa3f16c198dfa6229c
koki-develop <kou.pg.0131@gmail.com>
2023-09-16 09:26:52 +0900

    Add OpenAI chat completion to root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,56 @@
 package cmd
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
 	"os"
+	"strings"
 
+	openai "github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "CLI_NAME", // TODO
+	RunE: func(cmd *cobra.Command, args []string) error {
+		q := strings.Join(args, " ")
+
+		ctx := context.Background()
+
+		key := os.Getenv("OPENAI_API_KEY")
+		client := openai.NewClient(key)
+
+		// TODO: from config
+		model := openai.GPT3Dot5Turbo
+
+		stream, err := client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest{
+			Messages: []openai.ChatCompletionMessage{
+				{Role: openai.ChatMessageRoleUser, Content: q},
+			},
+			Model:  model,
+			Stream: true,
+		})
+		if err != nil {
+			return err
+		}
+		defer stream.Close()
+
+		for {
+			resp, err := stream.Recv()
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				return err
+			}
+
+			fmt.Print(resp.Choices[0].Delta.Content)
+		}
+
+		return nil
+	},
 }
 
 func Execute() {
