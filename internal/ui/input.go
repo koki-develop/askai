7ae1bcfe5c9ab35d8a1ed0d42bc22ac4692216bd
koki-develop <kou.pg.0131@gmail.com>
2023-09-16 13:02:42 +0900

    Create inputModel

---
+++ internal/ui/input.go
@@ -0,0 +1,61 @@
+package ui
+
+import (
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+var (
+	_ tea.Model = (*inputModel)(nil)
+)
+
+type inputModel struct {
+	abort    bool
+	quitting bool
+	value    string
+	textarea textarea.Model
+}
+
+func newInputModel() *inputModel {
+	ta := textarea.New()
+	ta.Placeholder = "Ask AI anything..."
+	ta.ShowLineNumbers = false
+	ta.Focus()
+
+	return &inputModel{
+		textarea: ta,
+	}
+}
+
+func (m *inputModel) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m *inputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			m.quitting = true
+			m.abort = true
+			return m, tea.Quit
+		case tea.KeyCtrlD:
+			m.quitting = true
+			m.value = m.textarea.Value()
+			return m, tea.Quit
+		}
+	}
+
+	m.textarea, cmd = m.textarea.Update(msg)
+	return m, cmd
+}
+
+func (m *inputModel) View() string {
+	if m.quitting {
+		return ""
+	}
+
+	return m.textarea.View()
+}
